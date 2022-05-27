9f9dacc2b8327716c7589cf468b9c98833cb2b37
Lesley Keyes <lesleyjkeyes@gmail.com>
2022-05-26 21:40:01 -0400

    finished assignment

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,67 @@
 // USE WITH FIREBASE AUTH
 // import checkLoginStatus from './helpers/auth';
+import axios from 'axios';
 import 'bootstrap'; // import bootstrap elements and js
+// import { render } from 'sass';
 import '../styles/main.scss';
 
-const init = () => {
-  document.querySelector('#app').innerHTML = `
-    <h1>HELLO! You are up and running!</h1>
-    <small>Open your dev tools</small><br />
-    <button class="btn btn-danger" id="click-me">Click ME!</button><br />
-    <hr />
-    <h2>These are font awesome icons:</h2>
-    <i class="fas fa-user fa-4x"></i> <i class="fab fa-github-square fa-5x"></i>
+const renderToDOM = (divId, content) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = content;
+};
+
+const htmlStructure = () => {
+  const domString = `
+  <div id ="form-container"></div>
+  <div id="lyrics-container"></div>
   `;
-  console.warn('YOU ARE UP AND RUNNING!');
+  renderToDOM('#app', domString);
+};
 
-  document
-    .querySelector('#click-me')
-    .addEventListener('click', () => console.warn('You clicked that button!'));
+const getLyrics = (artist, song) => new Promise((resolve, reject) => {
+  axios.get(`https://api.lyrics.ovh/v1/${artist}/${song}`)
+    .then((response) => resolve(response.data))
+    .catch((error) => reject(error));
+});
 
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
+const lyricsOnDom = (artist, song) => {
+  getLyrics(artist, song).then((response) => {
+    renderToDOM('#lyrics-container', response.lyrics);
+  });
+};
+
+const formOnDom = () => {
+  const domString = `
+  <form id = "form">
+  <div class="mb-3">
+    <label for="artist" class="form-label">Artist</label>
+    <input type="text" class="form-control" id="artist">
+  </div>
+  <div class="mb-3">
+    <label for="title" class="form-label">Song Title</label>
+    <input type="text" class="form-control" id="songTitle" aria-describedby="emailHelp">
+  </div>
+  <button type="submit" class="btn btn-primary">Find Lyrics</button>
+</form>
+  `;
+  renderToDOM('#form-container', domString);
+};
+
+const renderUserLyrics = () => {
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const inputArtist = document.querySelector('#artist').value;
+    const inputSong = document.querySelector('#songTitle').value;
+    lyricsOnDom(inputArtist, inputSong);
+    form.reset();
+  });
+};
+
+const init = () => {
+  htmlStructure();
+  formOnDom();
+  renderUserLyrics();
 };
 
 init();
